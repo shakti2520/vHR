ae21bf6b99f4d845de387bc399edbb22445a0a9a
shakti2520 <shaktisingh2520@gmail.com>
2020-02-22 22:29:59 +0530

    dashboard outline added

--- resume parser/app.py
+++ resume parser/app.py
@@ -16,5 +16,13 @@ c = conn.cursor()
 def home():
     return render_template('home.html')
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
 if __name__ == '__main__':
     app.run(debug = True, use_reloader = False)
